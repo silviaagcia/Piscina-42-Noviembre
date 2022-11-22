56bb3eaebaeaec623fe39e4b174863b5d98c1409
Silvia Angulo Garcia <siangulo@c2r8s3.42madrid.com>
2022-11-22 20:12:42 +0100

    get char

---
+++ BSQ/ft_get_chars.c
@@ -0,0 +1,34 @@
+#include 
+
+char	ft_get_char_empty(char *argv)
+{
+	int	i;
+	char	*buf;
+	int	fd;
+	char	e;
+
+	i = 0;
+	fd = open(argv, O_RDONLY);
+}
+
+char	ft_get_char_obstacle(char *argv)
+{
+	int	i;
+	char	*buf;
+	int	fd;
+	char	o;
+
+	i = 0;
+	fd = open(argv, O_RDONLY);
+}
+
+char	ft_get_char_full(char *argv)
+{
+	int	i;
+	char	*buf;
+	int	fd;
+	char	f;
+
+	i = 0;
+	fd = open(argv, O_RDONLY);
+}
