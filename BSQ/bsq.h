2af428dda50101a44e805c235ad59ee599ccb0e6
Silvia Angulo Garcia <siangulo@c2r8s3.42madrid.com>
2022-11-22 21:04:00 +0100

    biblioteca

---
+++ BSQ/bsq.h
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+
+char	ft_get_char_empty(char *argv);
+char	ft_get_char_obstacle(char *argv);
+char	ft_get_char_full(char *argv);
+int	ft_str_is_numeric(char *str);
+int	ft_strlen(char *str);
+void	ft_div(int x);
+void	ft_putnbr(int nb);
+char	*ft_strncpy(char *dest, char *src, unsigned int n);
+
